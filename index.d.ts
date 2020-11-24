419de849f3e5b5ebef28445eff3d23e3d45c7ec3
anurag shivarathri <anuragindianbraves@gmail.com>
2020-11-24 10:52:32 +0530

    Added types for ts

---
+++ index.d.ts
@@ -0,0 +1,37 @@
+interface IGenericObject {
+    [key: string]: any;
+}
+
+type IReducerFn = (
+    state: IGenericObject,
+    action: {
+        type: string;
+        payload: any[] | IGenericObject;
+        [key: string]: any
+    }
+) => IGenericObject;
+
+type IReducer = IGenericObject | IReducerFn;
+
+declare module 'ninja-store' {
+
+    export function createStore(
+        name: string,
+        reducer: IReducer,
+        initialState?: IGenericObject,
+        timeoutDuration?: number
+    ): {
+        useStore<T>(
+            mapFn?: (state: T) => IGenericObject
+        ): [
+            state: T,
+            setState?: (
+                action?: string | 
+                ((state: T) => T) | 
+                T
+            ) => void,
+            getState ?: () => T
+        ]
+    }
+
+}
